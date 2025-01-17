1c6d9756607e94af7dbd50b9c96a92e9f5fd284e
Lauryn C <65569546+crandalllauryn@users.noreply.github.com>
2025-01-17 18:14:07 -0500

    Update excel_to_dic.py

--- excel_to_dic.py
+++ excel_to_dic.py
@@ -1,255 +1,79 @@
-# imports
-
+# Imports
 import pandas as pd
-
 from datetime import datetime
-
 from matplotlib import pyplot as plt
-
-from pprint import pprint
-
 import re
 
- 
-
-# current date and time
-
+# Current date and time
 now = datetime.now()
 
- 
-
-# plot set up
-
+# Plot setup
 plt.style.use('seaborn-whitegrid')
-
 plt.rcParams["figure.autolayout"] = True
 
- 
-
+# Path to the Excel file
 path = r'C:\Users\223050503\Downloads\Mission 2.xlsx'
 
- 
-
-# strip emojis
-
-emoji_pattern = re.compile("["
-
-                           u"\U0001F600-\U0001F64F"  # emoticons
-
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-
-                           "]+", flags=re.UNICODE)
-
-clean_task_name = []
-
- 
-
-# all label names
-
- 
-
-# Ident - Enter
-
-# Constraints
-
-# Ident - Delete
-
-# Disco Entry
-
-# Enter Waypoint List
-
-# Pending GPM Run
-
- 
-
-# Pull Teams Excel Data
-
-df = pd.read_excel(path, index_col=0, header=0)
-
-df2 = df.fillna(value=0)
-
- 
-
-task_name = list(df2.iloc[:, 0])
-
-for i in range(len(task_name)):
-
-    clean_task_name.append(emoji_pattern.sub(r'', task_name[i]))
-
- 
-
-bucket_data = list(df2.iloc[:, 1])
-
-pull_created_date = list(df2.iloc[:, 6])
-
-pull_completed_date = list(df2.iloc[:, 10])
-
-label_data = list(df2.iloc[:, 15])
-
- 
-
-# Pre-define your dictionaries and lists
-
- 
-
-done_avg_dic = {}
-
-wip_avg_dic = {}
-
-wip_dic = {}
-
- 
-
-x = []
-
-y = []
-
- 
-
-# Loop to organize done data
-
- 
-
-for i in range(len(task_name)):
-
-    if pull_completed_date[i] == 0:
-
-        pull_completed_date[i] = pull_created_date[i]
-
-    a = datetime.strptime(pull_created_date[i], '%m/%d/%Y')
-
-    b = datetime.strptime(pull_completed_date[i], '%m/%d/%Y')
-
-    cycle_time = b-a
-
-    cycle_time = cycle_time.days
-
-    if cycle_time > 0:
-
-        if bucket_data[i] == 'Done':
-
-            x.append(b)
-
-            y.append(cycle_time)
-
-            done_avg_dic[i] = cycle_time
-
- 
-
-# find done avg
-
-done_avg = 0
-
- 
-
-for val in done_avg_dic.values():
-
-    done_avg += val
-
- 
-
-done_avg = done_avg // len(done_avg_dic)
-
- 
-
-# plot done tasks
-
-ax = plt.subplot()
-
- 
-
+# Regex to strip emojis
+emoji_pattern = re.compile(
+    "["
+    u"\U0001F600-\U0001F64F"  # Emoticons
+    u"\U0001F300-\U0001F5FF"  # Symbols & pictographs
+    u"\U0001F680-\U0001F6FF"  # Transport & map symbols
+    u"\U0001F1E0-\U0001F1FF"  # Flags (iOS)
+    "]+", flags=re.UNICODE
+)
+
+# Read and clean Excel data
+df = pd.read_excel(path, index_col=0, header=0).fillna(value=0)
+
+# Extract relevant columns
+task_names = [emoji_pattern.sub('', name) for name in df.iloc[:, 0]]
+bucket_data = list(df.iloc[:, 1])
+created_dates = list(df.iloc[:, 6])
+completed_dates = list(df.iloc[:, 10])
+label_data = list(df.iloc[:, 15])
+
+# Pre-define dictionaries and lists
+done_avg_dic, wip_avg_dic = {}, {}
+x, y = [], []
+
+# Process "Done" tasks and calculate cycle times
+for i, name in enumerate(task_names):
+    if completed_dates[i] == 0:
+        completed_dates[i] = created_dates[i]
+    
+    start_date = datetime.strptime(created_dates[i], '%m/%d/%Y')
+    end_date = datetime.strptime(completed_dates[i], '%m/%d/%Y')
+    cycle_time = (end_date - start_date).days
+
+    if cycle_time > 0 and bucket_data[i] == 'Done':
+        x.append(end_date)
+        y.append(cycle_time)
+        done_avg_dic[i] = cycle_time
+
+# Calculate average cycle time for "Done" tasks
+done_avg = sum(done_avg_dic.values()) // len(done_avg_dic)
+
+# Plot "Done" tasks cycle times
 plt.plot(x, y, 'o')
-
- 
-
-ax.axhline(done_avg, label='Average Cycle Time of Completed items', linestyle='--')
-
- 
-
-plt.legend(loc='upper right')
-
-plt.title('Cycle Time Chart of Completed items')
-
+plt.axhline(done_avg, label='Average Cycle Time (Done)', linestyle='--')
+plt.title('Cycle Time Chart of Completed Items')
 plt.xlabel('Completion Date')
-
-plt.ylabel('Cycle Time Hours')
-
- 
-
+plt.ylabel('Cycle Time (Days)')
+plt.legend(loc='upper right')
 plt.show()
 
- 
+print(f"Done cycle time average: {done_avg} days")
 
-print("Our done cycle time average is: " + str(done_avg) + " days")
+# Process WIP tasks
+for i, name in enumerate(task_names):
+    start_date = datetime.strptime(created_dates[i], '%m/%d/%Y')
+    wip_time = (now - start_date).days
 
- 
+    if bucket_data[i] in ['In Work', 'Review']:
+        wip_avg_dic[i] = wip_time
 
-# x = []
-
-# y = []
-
- 
-
-# for next team
-
-# for i in range(len(task_name)):
-
-#     if pull_completed_date[i] == 0:
-
-#         pull_completed_date[i] = pull_created_date[i]
-
-#     a = datetime.strptime(pull_created_date[i], '%m/%d/%Y')
-
-#     b = datetime.strptime(pull_completed_date[i], '%m/%d/%Y')
-
-#     cycle_time = b-a
-
-#     cycle_time = cycle_time.days
-
-#     if cycle_time > 0 and bucket_data[i] == 'Done':
-
-#         x.append(b)
-
-#         y.append(cycle_time)
-
-#         done_avg_dic[i] = cycle_time
-
- 
-
-# for all wip items
-
-for i in range(len(task_name)):
-
-    a = datetime.strptime(pull_created_date[i], '%m/%d/%Y')
-
-    wip = now - a
-
-    wip = wip.days
-
-    if bucket_data[i] == 'In Work' or bucket_data[i] == 'Review':
-
-        wip_avg_dic[i] = wip
-
- 
-
-# Find WIP Average Cycle Time
-
-wip_avg = 0
-
- 
-
-for val in wip_avg_dic.values():
-
-    wip_avg += val
-
- 
-
-wip_avg = wip_avg // len(wip_avg_dic)
-
- 
-
-print("Our wip cycle time average is: " + str(wip_avg) + " days")
+# Calculate average cycle time for WIP tasks
+wip_avg = sum(wip_avg_dic.values()) // len(wip_avg_dic)
+print(f"WIP cycle time average: {wip_avg} days")
